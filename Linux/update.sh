91a484f61c7a6c2c84decba859b8279bb7fd8ae2
yangzhiyuanDrwells <51501986+yangzhiyuanDrwells@users.noreply.github.com>
2023-10-19 17:34:32 +0800

    Update update.sh

--- Linux/update.sh
+++ Linux/update.sh
@@ -3,4 +3,4 @@ ipv6_address=$(/duckdns/get_ipv6_re.sh)
 echo "$ipv6_address"
 # 输出获取的 IPv6 地址
 # echo "$ipv6_address" >> /duckdns/updatelog_yunshu.log 2>&1
-echo url="https://www.duckdns.org/update?domains=yunshu&token=a613170e-c9c5-4615-935e-12f9ea953b56&ipv6=$ipv6_address" | curl -k -o /duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=xxxx&token=xxxxxxxx-c9c5-4615-935e-12f9ea953b56&ipv6=$ipv6_address" | curl -k -o /duckdns/duck.log -K -
