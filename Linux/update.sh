a9b2f4778b98b79a101ef29f1979855b9b611565
yangzhiyuanDrwells <2582050678@qq.com>
2023-11-13 15:01:18 +0800

    update update.sh

--- Linux/update.sh
+++ Linux/update.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
-echo $(date "+%Y-%m-%d %H:%M:%S") >> ./duck.log  # echo current time to log
-ipv6_address=$(./get_ipv6_re.sh)  # get ipv6
-echo "$ipv6_address"
+# 获取脚本所在目录的相对路径
+SCRIPT_DIR=$(dirname "$0")
+# 获取脚本所在目录的绝对路径
+ABSOLUTE_PATH=$(cd "$SCRIPT_DIR" && pwd)
+ipv6_address=$($ABSOLUTE_PATH/get_ipv6_re.sh)  # get ipv6
+echo $(date "+%Y-%m-%d %H:%M:%S") >> $ABSOLUTE_PATH/duck.log  # echo current time to log
 # 输出获取的 IPv6 地址
-echo "$ipv6_address" >> ./duck.log 2>&1
-# 通过curl发起网络请求更新ddns
-domains=xxxxxxxx
-token=da2f0fce-ed39-4fd9-bbfa-xxxxxxxxxxxx
+echo "$ipv6_address" >> $ABSOLUTE_PATH/duck.log 2>&1
+# use curl update ddns
+domains=ubuntu224
+token=da2f0fce-ed39-4fd9-bbfa-0c63b61adab9
 url="url=https://www.duckdns.org/update?domains=$domains&token=$token&ipv6=$ipv6_address"
 echo "$url" > ./url
-curl -k -K ./url >> ./duck.log
-echo "\n" >> ./duck.log
+curl -k -K ./url >> $ABSOLUTE_PATH/duck.log
+echo "\n" >> $ABSOLUTE_PATH/duck.log
